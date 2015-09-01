3b5747c6c2f3203b86144a042350bd75c24bcb78
Kieran <kieran@Kierans-MacBook-Pro.local>
2015-09-01 10:27:43 +0100

    Positioned Asset sheet element, 1st draft of site complete, but still need to rectify some issues before moving onto interactivity

--- js/rwd.js
+++ js/rwd.js
@@ -1,73 +1,73 @@
-jQuery(document).ready(function($){
+// jQuery(document).ready(function($){
 
-  // Touch friendly expanded nav
-  $('ul.navmenu li span').click(function(event){
+//   // Touch friendly expanded nav
+//   $('ul.navmenu li span').click(function(event){
 
-    event.preventDefault();
+//     event.preventDefault();
 
-    // get the child of the clicked menu switch
-    var child_menu = $(this).parent().parent().children('ul');
+//     // get the child of the clicked menu switch
+//     var child_menu = $(this).parent().parent().children('ul');
 
-    // get the parent link of the clicked menu switch
-    var parent_link = $(this).parent();
+//     // get the parent link of the clicked menu switch
+//     var parent_link = $(this).parent();
 
-    // check if it's currently open or closed
-    if ( child_menu.hasClass('childopen') ) {
-      // if it's open, close it
+//     // check if it's currently open or closed
+//     if ( child_menu.hasClass('childopen') ) {
+//       // if it's open, close it
 
-      // remove any "active" class from parent item
-      $(parent_link).removeClass('active');
-      // hide child menu
-      $(child_menu).removeClass('childopen');
-      // set this open menu switch to +
-      $(this).html('+');
+//       // remove any "active" class from parent item
+//       $(parent_link).removeClass('active');
+//       // hide child menu
+//       $(child_menu).removeClass('childopen');
+//       // set this open menu switch to +
+//       $(this).html('+');
 
-    } else {
-      // if it's closed, open it
+//     } else {
+//       // if it's closed, open it
 
-      // hide any open child menus
-      $('ul.navmenu ul').removeClass('childopen');
-      // set any open menu switch symbols back to +
-      $('ul.navmenu li span').html('+');
-      // remove any existing "active" class from menus
-      $('.active').removeClass('active');
+//       // hide any open child menus
+//       $('ul.navmenu ul').removeClass('childopen');
+//       // set any open menu switch symbols back to +
+//       $('ul.navmenu li span').html('+');
+//       // remove any existing "active" class from menus
+//       $('.active').removeClass('active');
 
-      // add "active" class to this parent item
-      $(parent_link).addClass('active');
-      // show correct child menu
-      $(child_menu).addClass('childopen');
-      // set this open menu switch to -
-      $(this).html('-');
+//       // add "active" class to this parent item
+//       $(parent_link).addClass('active');
+//       // show correct child menu
+//       $(child_menu).addClass('childopen');
+//       // set this open menu switch to -
+//       $(this).html('-');
 
-      return false;
+//       return false;
 
-    }
+//     }
 
-  });
+//   });
 
-  $('.navicon').click(function(){
+//   $('.navicon').click(function(){
     
-    if ( $('.navmenu').css('display') == 'none' ) {
+//     if ( $('.navmenu').css('display') == 'none' ) {
 
-      $('.navmenu').addClass('show');
-      $('.navicon').removeClass('closed').addClass('open');
-      $('.navicon').children('.fa').removeClass('fa-navicon').addClass('fa-close');
+//       $('.navmenu').addClass('show');
+//       $('.navicon').removeClass('closed').addClass('open');
+//       $('.navicon').children('.fa').removeClass('fa-navicon').addClass('fa-close');
 
-    } else {
+//     } else {
 
-      $('.navmenu').removeClass('show');
-      $('.navicon').removeClass('open').addClass('closed');
-      $('.navicon').children('.fa').removeClass('fa-close').addClass('fa-navicon');
+//       $('.navmenu').removeClass('show');
+//       $('.navicon').removeClass('open').addClass('closed');
+//       $('.navicon').children('.fa').removeClass('fa-close').addClass('fa-navicon');
 
-    }
+//     }
 
-  });
+//   });
 
   //Stripes jQuery
   $(function() {
   $(window).resize(function() {
     var extra = $('.strip-parent').height() * Math.tan(20);
-    $('.strip-parent > .strip').css('left', extra / 2);
+    // $('.strip-parent > .strip').css('left', extra / 2);
     $('.strip-parent > .strip > .bg-img').css('left', -(extra / 2)).width('calc(100% + ' + extra + 'px');
   }).resize();
 });
