9184ebbb75d7cbbc2d93fa3e66e3ac398391b42c
kieranmcfarlane <kieranmcfarlane2@googlemail.com>
2015-09-14 14:09:32 +0100

    Delete morph

--- js/rwd.js
+++ js/rwd.js
@@ -52,75 +52,14 @@
    }); 
 
 /*
- * Morph button code
+ * Register popup
  */
 
-   /* Invite button */
-  var docElem = window.document.documentElement, didScroll, scrollPosition;
+  
 
-  // trick to prevent scrolling when opening/closing button
-  function noScrollFn() {
-    window.scrollTo( scrollPosition ? scrollPosition.x : 0, scrollPosition ? scrollPosition.y : 0 );
-  }
-
-  function noScroll() {
-    window.removeEventListener( 'scroll', scrollHandler );
-    window.addEventListener( 'scroll', noScrollFn );
-  }
-
-  function scrollFn() {
-    window.addEventListener( 'scroll', scrollHandler );
-  }
-
-  function canScroll() {
-    window.removeEventListener( 'scroll', noScrollFn );
-    scrollFn();
-  }
-
-  function scrollHandler() {
-    if( !didScroll ) {
-      didScroll = true;
-      setTimeout( function() { scrollPage(); }, 60 );
-    }
-  };
-
-  function scrollPage() {
-    scrollPosition = { x : window.pageXOffset || docElem.scrollLeft, y : window.pageYOffset || docElem.scrollTop };
-    didScroll = false;
-  };
-
-  scrollFn();
-
-  [].slice.call( document.querySelectorAll( '.morph-button, .nate' )).forEach( function( bttn ) {
-    morphingButton = new UIMorphingButton( bttn, {
-      closeEl : '.icon-close',
-      onBeforeOpen : function() {
-        // don't allow to scroll
-        noScroll();
-      },
-      onAfterOpen : function() {
-        // can scroll again
-        canScroll();
-      },
-      onBeforeClose : function() {
-        // don't allow to scroll
-        noScroll();
-      },
-      onAfterClose : function() {
-        // can scroll again
-        canScroll();
-      }
-    } );
-  } );
-
-  morphButton = $('.morph-button');
-
-  $('.open-login-morph').on('click', function(e) {
-      morphingButton.toggle();
-      e.preventDefault();
-  });
-
-
+  /*
+   * Jump to
+   */
 
   var jumpTo = $('.jump-to'),
       jumpToButton = $('.jump-to-button'),
